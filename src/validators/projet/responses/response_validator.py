9fcad82ce752c3bdd3ae006c399682e503787aa7
lucas.vittaz <lucasvittaz.pro@gmail.com>
2024-09-01 21:58:36 +0200

    responses validators + tests

--- src/validators/projet/responses/response_validator.py
+++ src/validators/projet/responses/response_validator.py
@@ -1,41 +1,11 @@
 from ..base_validator import BaseValidator
 
 class ResponseValidator(BaseValidator):
-    """
-    Valide les réponses définies dans le Swagger en fonction des règles spécifiques pour chaque méthode HTTP,
-    en tenant compte des références ($ref) dans les schémas.
-    """
-
     def __init__(self, swagger_dict, swagger_text, rules):
-        """
-        Initialise le validateur avec les règles de validation des réponses pour chaque méthode HTTP.
-        
-        :param swagger_dict: Dictionnaire contenant la représentation du fichier Swagger.
-        :param swagger_text: Chaîne de caractères contenant le texte brut du fichier Swagger.
-        :param rules: Règles spécifiques pour chaque méthode HTTP.
-        """
         super().__init__(swagger_dict, swagger_text)
         self.rules = rules
 
-    def resolve_reference(self, ref):
-        """
-        Résout une référence $ref dans le Swagger.
-
-        :param ref: Le chemin de la référence sous la forme '#/components/schemas/SchemaName'.
-        :return: Le schéma référencé sous forme de dictionnaire.
-        """
-        ref_path = ref.lstrip('#/').split('/')
-        resolved = self.swagger_dict
-        for part in ref_path:
-            resolved = resolved.get(part, {})
-        return resolved
-
     def validate_responses(self):
-        """
-        Valide les réponses dans le Swagger en fonction des règles spécifiées pour chaque méthode HTTP.
-        
-        :return: Une liste d'erreurs trouvées lors de la validation des réponses.
-        """
         errors = []
         paths = self.swagger_dict.get('paths', {})
 
@@ -53,23 +23,10 @@ class ResponseValidator(BaseValidator):
                             actual_schema = actual_response.get("content", {}).get(content_type, {}).get("schema", {})
                             expected_schema = expected_response["format"]
 
-                            if "$ref" in actual_schema:
-                                actual_schema = self.resolve_reference(actual_schema["$ref"])
-
                             errors.extend(self._validate_response_schema(actual_schema, expected_schema, response_code, method, path))
         return errors
 
     def _validate_response_schema(self, actual_schema, expected_schema, response_code, method, path):
-        """
-        Valide le schéma d'une réponse en fonction d'un schéma attendu.
-
-        :param actual_schema: Le schéma réel extrait de la réponse Swagger.
-        :param expected_schema: Le schéma attendu selon les règles.
-        :param response_code: Le code de la réponse (200, 404, etc.).
-        :param method: La méthode HTTP pour laquelle cette réponse est utilisée.
-        :param path: Le chemin d'API pour lequel cette réponse est utilisée.
-        :return: Une liste d'erreurs si la validation échoue.
-        """
         errors = []
 
         def format_rule():
@@ -78,21 +35,24 @@ class ResponseValidator(BaseValidator):
                 f"{expected_schema}\n"
             )
 
-        if expected_schema.get("type") and actual_schema.get("type") != expected_schema["type"]:
+        actual_type = actual_schema.get("type")
+        expected_type = expected_schema.get("type")
+
+        if expected_type and actual_type != expected_type:
+            # Ne pas signaler d'erreur si le schéma attendu est vide
+            if not actual_schema and expected_schema == {}:
+                return errors
             errors.append(
-                f"Le type de la réponse pour le code '{response_code}' dans {method.upper()} {path} est '{actual_schema.get('type')}', "
-                f"mais il devrait être '{expected_schema['type']}'.\n{format_rule()}"
+                f"Le type de la réponse pour le code '{response_code}' dans {method.upper()} {path} est '{actual_type}', "
+                f"mais il devrait être '{expected_type}'.\n{format_rule()}"
             )
 
         if expected_schema.get("properties"):
             for prop, prop_expected_schema in expected_schema.get("properties", {}).items():
-                if prop not in actual_schema.get("properties", {}):
+                actual_prop_schema = actual_schema.get("properties", {}).get(prop)
+                if not actual_prop_schema:
                     errors.append(f"Le champ '{prop}' est manquant dans la réponse pour le code '{response_code}' dans {method.upper()} {path}.\n{format_rule()}")
                 else:
-                    actual_prop_schema = actual_schema.get("properties", {}).get(prop)
-                    if "$ref" in actual_prop_schema:
-                        actual_prop_schema = self.resolve_reference(actual_prop_schema["$ref"])
-
                     if prop_expected_schema.get("type") and actual_prop_schema.get("type") != prop_expected_schema["type"]:
                         errors.append(
                             f"Le type du champ '{prop}' dans la réponse pour le code '{response_code}' dans {method.upper()} {path} est '{actual_prop_schema.get('type')}', "
@@ -101,8 +61,6 @@ class ResponseValidator(BaseValidator):
 
         if expected_schema.get("items"):
             actual_items_schema = actual_schema.get("items", {})
-            if "$ref" in actual_items_schema:
-                actual_items_schema = self.resolve_reference(actual_items_schema["$ref"])
 
             errors.extend(self._validate_response_schema(actual_items_schema, expected_schema["items"], response_code, method, path))
 
--- tests/test_responses/test_response_validator.py
+++ tests/test_responses/test_response_validator.py
@@ -2,19 +2,8 @@ import pytest
 from src.validators.projet.responses.response_validator import ResponseValidator
 
 @pytest.fixture
-def swagger_responses_with_refs():
+def swagger_responses():
     return {
-        "components": {
-            "schemas": {
-                "ErrorResponse": {
-                    "type": "object",
-                    "properties": {
-                        "error": {"type": "string"},
-                        "error_description": {"type": "string"}
-                    }
-                }
-            }
-        },
         "paths": {
             "/example": {
                 "get": {
@@ -29,37 +18,6 @@ def swagger_responses_with_refs():
                                             "message": {"type": "string"}
                                         }
                                     }
-                                }
-                            }
-                        },
-                        "400": {
-                            "description": "Requête incorrecte",
-                            "content": {
-                                "application/json": {
-                                    "schema": {
-                                        "$ref": "#/components/schemas/ErrorResponse"
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-@pytest.fixture
-def swagger_responses_with_empty_objects():
-    return {
-        "paths": {
-            "/example": {
-                "get": {
-                    "responses": {
-                        "200": {
-                            "description": "Requête réussie",
-                            "content": {
-                                "application/json": {
-                                    "schema": {}
                                 }
                             }
                         },
@@ -117,24 +75,125 @@ def rules():
         }
     }
 
-def test_valid_responses_with_refs(swagger_responses_with_refs, rules):
-    validator = ResponseValidator(swagger_responses_with_refs, "", rules)
+def test_valid_responses(swagger_responses, rules):
+    validator = ResponseValidator(swagger_responses, "", rules)
     errors = validator.validate_responses()
-    assert not errors, f"Aucune erreur ne devrait être trouvée pour des réponses valides avec références, mais les erreurs suivantes ont été trouvées : {errors}"
+    assert not errors, f"Aucune erreur ne devrait être trouvée pour des réponses valides, mais les erreurs suivantes ont été trouvées : {errors}"
 
-def test_valid_responses_with_empty_objects(swagger_responses_with_empty_objects, rules):
+def test_invalid_response_type(swagger_responses, rules):
+    swagger_responses["paths"]["/example"]["get"]["responses"]["200"]["content"]["application/json"]["schema"]["type"] = "array"
+    validator = ResponseValidator(swagger_responses, "", rules)
+    errors = validator.validate_responses()
+    assert errors, "Une erreur devrait être trouvée pour un type de réponse incorrect."
+
+def test_missing_response(swagger_responses, rules):
+    del swagger_responses["paths"]["/example"]["get"]["responses"]["200"]
+    validator = ResponseValidator(swagger_responses, "", rules)
+    errors = validator.validate_responses()
+    assert errors, "Une erreur devrait être trouvée pour une réponse manquante."
+
+def test_valid_responses_with_empty_objects():
+    swagger_responses_with_empty_objects = {
+        "paths": {
+            "/example": {
+                "get": {
+                    "responses": {
+                        "200": {
+                            "description": "Requête réussie",
+                            "content": {
+                                "application/json": {
+                                    "schema": {}
+                                }
+                            }
+                        },
+                        "400": {
+                            "description": "Requête incorrecte",
+                            "content": {
+                                "application/json": {
+                                    "schema": {
+                                        "type": "array",
+                                        "items": {
+                                            "type": "object",
+                                            "properties": {
+                                                "error": {"type": "string"},
+                                                "error_description": {"type": "string"}
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    rules = {
+        "GET": {
+            "responses": [
+                {
+                    "response_code": 200,
+                    "format": {}
+                },
+                {
+                    "response_code": 400,
+                    "format": {
+                        "type": "array",
+                        "items": {
+                            "type": "object",
+                            "properties": {
+                                "error": {"type": "string"},
+                                "error_description": {"type": "string"}
+                            }
+                        }
+                    }
+                }
+            ]
+        }
+    }
     validator = ResponseValidator(swagger_responses_with_empty_objects, "", rules)
     errors = validator.validate_responses()
     assert not errors, f"Aucune erreur ne devrait être trouvée pour des réponses valides avec des objets vides ou des tableaux d'objets, mais les erreurs suivantes ont été trouvées : {errors}"
 
-def test_invalid_ref_resolution(swagger_responses_with_refs, rules):
-    swagger_responses_with_refs["paths"]["/example"]["get"]["responses"]["400"]["content"]["application/json"]["schema"]["$ref"] = "#/components/schemas/NonExistentSchema"
-    validator = ResponseValidator(swagger_responses_with_refs, "", rules)
+def test_invalid_response_format():
+    swagger_responses_invalid = {
+        "paths": {
+            "/example": {
+                "get": {
+                    "responses": {
+                        "200": {
+                            "description": "Requête réussie",
+                            "content": {
+                                "application/json": {
+                                    "schema": {
+                                        "type": "object",
+                                        "properties": {
+                                            "message": {"type": "integer"}  # Type incorrect
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    rules = {
+        "GET": {
+            "responses": [
+                {
+                    "response_code": 200,
+                    "format": {
+                        "type": "object",
+                        "properties": {
+                            "message": {"type": "string"}
+                        }
+                    }
+                }
+            ]
+        }
+    }
+    validator = ResponseValidator(swagger_responses_invalid, "", rules)
     errors = validator.validate_responses()
-    assert errors, "Une erreur devrait être trouvée pour une référence non résolue."
-
-def test_missing_response(swagger_responses_with_refs, rules):
-    del swagger_responses_with_refs["paths"]["/example"]["get"]["responses"]["200"]
-    validator = ResponseValidator(swagger_responses_with_refs, "", rules)
-    errors = validator.validate_responses()
-    assert errors, "Une erreur devrait être trouvée pour une réponse manquante."
+    assert errors, "Une erreur devrait être trouvée pour un format de réponse incorrect."
